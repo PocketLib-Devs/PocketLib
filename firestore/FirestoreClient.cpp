7a19b139c876394e9deb86784f6afdbbaf9d9aab
AishwaryaIITJ <b25ee1003@iitj.ac.in>
2026-03-14 04:22:22 +0530

    Added firestore config for authentication

---
+++ firestore/FirestoreClient.cpp
@@ -0,0 +1,109 @@
+#include "FirestoreClient.h"
+
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QJsonDocument>
+#include <QJsonObject>
+
+FirestoreClient::FirestoreClient(QString projectId, QObject *parent)
+    : QObject(parent)
+{
+    manager = new QNetworkAccessManager(this);
+
+    baseUrl =
+    "https://firestore.googleapis.com/v1/projects/" +
+    projectId +
+    "/databases/(default)/documents/";
+}
+
+void FirestoreClient::setAuthToken(QString token)
+{
+    idToken = token;
+}
+
+void FirestoreClient::addBook(QString title, QString author, int copies)
+{
+    QJsonObject fields;
+
+    fields["title"] =
+        QJsonObject{{"stringValue", title}};
+
+    fields["author"] =
+        QJsonObject{{"stringValue", author}};
+
+    fields["availableCopies"] =
+        QJsonObject{{"integerValue", QString::number(copies)}};
+
+    QJsonObject body;
+    body["fields"] = fields;
+
+    QJsonDocument doc(body);
+
+    QNetworkRequest request(baseUrl + "books");
+
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+    request.setRawHeader("Authorization",
+                         ("Bearer " + idToken).toUtf8());
+
+    QNetworkReply *reply =
+        manager->post(request, doc.toJson());
+
+    connect(reply, &QNetworkReply::finished, [=]() {
+
+        if(reply->error())
+            emit requestError(reply->errorString());
+        else
+            emit requestSuccess("Book added");
+
+        reply->deleteLater();
+    });
+}
+
+void FirestoreClient::getBooks()
+{
+    QNetworkRequest request(baseUrl + "books");
+
+    request.setRawHeader("Authorization",
+                         ("Bearer " + idToken).toUtf8());
+
+    QNetworkReply *reply = manager->get(request);
+
+    connect(reply, &QNetworkReply::finished, [=]() {
+
+        if(reply->error())
+        {
+            emit requestError(reply->errorString());
+            reply->deleteLater();
+            return;
+        }
+
+        QByteArray data = reply->readAll();
+
+        QJsonDocument doc = QJsonDocument::fromJson(data);
+        QJsonArray docs = doc.object()["documents"].toArray();
+
+        emit booksReceived(docs);
+
+        reply->deleteLater();
+    });
+}
+
+void FirestoreClient::deleteBook(QString documentId)
+{
+    QNetworkRequest request(baseUrl + "books/" + documentId);
+
+    request.setRawHeader("Authorization",
+                         ("Bearer " + idToken).toUtf8());
+
+    QNetworkReply *reply = manager->deleteResource(request);
+
+    connect(reply, &QNetworkReply::finished, [=]() {
+
+        if(reply->error())
+            emit requestError(reply->errorString());
+        else
+            emit requestSuccess("Book deleted");
+
+        reply->deleteLater();
+    });
+}
